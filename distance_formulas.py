325a920da93d37acee00089701ed26a6ee583116
Matt Hartman <hartman@my.ccsu.edu>
2022-03-21 15:58:32 -0400

    Renamed and added manhattan distance

---
+++ distance_formulas.py
@@ -0,0 +1,21 @@
+def euclidean_distance(pt1, pt2):
+    distance = 0
+    for i in range(len(pt1)):
+        distance += (pt1[i] - pt2[i]) ** 2
+    distance **= 0.5
+    return distance
+
+def manhattan_distance(pt1, pt2):
+    distance = 0
+    for i in range(len(pt1)):
+        distance += abs(pt1[i] - pt2[i])
+    return distance
+
+d2 = [[1, 2], [4, 0]]
+d3 = [[5, 4, 3], [1, 7, 9]]
+
+print(euclidean_distance(d2[0], d2[1]))
+print(euclidean_distance(d3[0], d3[1]))
+
+print(manhattan_distance(d2[0], d2[1]))
+print(manhattan_distance(d3[0], d3[1]))
--- euclidean_distance.py
+++
@@ -1,9 +0,0 @@
-def euclidean_distance(pt1, pt2):
-    distance = 0
-    for i in range(len(pt1)):
-        distance += (pt1[i] - pt2[i]) ** 2
-    distance **= 0.5
-    return distance
-
-print(euclidean_distance([1,2], [4,0]))
-print(euclidean_distance([5, 4, 3], [1, 7, 9]))
