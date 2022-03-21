999a2603b2719bc7d907eb1d7a140c3452974b8e
Matt Hartman <hartman@my.ccsu.edu>
2022-03-21 16:03:07 -0400

    Added hamming distance.

--- distance_formulas.py
+++ distance_formulas.py
@@ -11,6 +11,13 @@ def manhattan_distance(pt1, pt2):
         distance += abs(pt1[i] - pt2[i])
     return distance
 
+def hamming_distance(pt1, pt2):
+    distance = 0
+    for i in range(len(pt1)):
+        if pt1[i] != pt2[i]:
+            distance += 1
+    return distance
+
 d2 = [[1, 2], [4, 0]]
 d3 = [[5, 4, 3], [1, 7, 9]]
 
@@ -19,3 +26,9 @@ print(euclidean_distance(d3[0], d3[1]))
 
 print(manhattan_distance(d2[0], d2[1]))
 print(manhattan_distance(d3[0], d3[1]))
+
+print(hamming_distance(d2[0], d2[1]))
+print(hamming_distance(d3[0], d3[1]))
+
+print(hamming_distance([1, 2], [1, 100]))
+print(hamming_distance([5, 4, 9], [1, 7, 9]))
